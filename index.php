24028484ba50278a8b1fadaf7b8d562786b63c3b
leqviet <leviet2510@gmail.com>
2018-11-19 16:59:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+define('APPLICATION_PATH', 
+              realpath(dirname(__FILE__) . '/application')); 
+define('APPLICATION_ENV','production'); 
+set_include_path(APPLICATION_PATH . '/../library'); 
+require_once 'Zend/Application.php' ;
+require_once 'PHPExcel.php' ;
+$application = new Zend_Application( 
+    APPLICATION_ENV, 
+    APPLICATION_PATH . '/configs/application.ini' 
+); 
+$application->bootstrap()->run();
+
